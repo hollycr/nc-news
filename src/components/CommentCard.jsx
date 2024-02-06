5161db53967697f3b374cafa48ac80e5e62d7b6c
Holly Rees <hollyclarerees@gmail.com>
2024-02-06 11:46:26 +0000

    add delete comment functionality, and update post comment so that feedback message changes back to empty on blur

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,12 +1,38 @@
+import { useContext, useState } from "react";
+import UserContext from "../context/UserContext";
+
+import { deleteComment } from "../api/comments";
+
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 
-function CommentCard({ comment }) {
+function CommentCard({ comment, setCommentsChange }) {
+  const { loggedInUser } = useContext(UserContext);
+  const [commentId, setCommentId] = useState(null);
+
+  function handleDelete(event) {
+    deleteComment(event.target.value).then(() => {
+      setCommentsChange(true);
+    });
+    setCommentsChange(false);
+  }
+
   return (
     <Card variant="outlined">
       <h5>{comment.author}</h5>
       <p>{comment.body}</p>
-      <p>votes: {comment.votes}</p>
+      <div style={{ display: "inline-flex", alignItems: "center" }}>
+        <p style={{ marginRight: "10px" }}>votes: {comment.votes} </p>
+        {comment.author === loggedInUser.username ? (
+          <button
+            value={comment.comment_id}
+            style={{ backgroundColor: "#e4e4e4" }}
+            onClick={handleDelete}
+          >
+            delete comment
+          </button>
+        ) : null}
+      </div>
     </Card>
   );
 }
